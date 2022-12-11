a7d47b5d97049b948bd2418b5e81b9ef6243ef34
Coin Data School <coindataschool@gmail.com>
2022-12-11 13:23:01 +0900

    add text to reminder user to enter input numbers as precise as possible

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ st.markdown("""---""")
 
 # allow user to input values and get predictions
 st.header('Predict by entering your own values')
+st.markdown("Make sure enter numbers as precise as possible. For example, a price range of 28.1 - 39.8 can result in a very different prediction than a range of 28.1213 - 39.8134.")
 
 # user input
 c1, c2, c3, c4 = st.columns(4)
